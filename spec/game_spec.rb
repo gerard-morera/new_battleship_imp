f9dc1ef61ffe4c0e1723de8f878a59daa4e94172
Gerard Morera <grdmorera@gmail.com>
2015-06-18 11:13:12 +0100

    failed game class tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,18 @@
+require 'game'
+
 describe Game do
 
+	subject { Game.new }
+
+	let( :Theo   )
+	let( :Gerard ) 
+	let( :board  ) { double board }
+	let( :user   ) { double user, :name= => nil }
   describe "run_game" do
 
     it "creates two users" do
+    	subject.run_game Theo, Gerard
+    	expect( )
 
     end
 
