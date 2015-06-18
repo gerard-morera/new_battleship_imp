60fd9ecba1897b7a9bd006a13cde9a485007f6cb
Gerard Morera <grdmorera@gmail.com>
2015-06-18 12:38:12 +0100

    board hit test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,8 +5,16 @@ describe Board do
   it "can have a ship" do
     ship = double :ship
     subject.place ship
-    expect(subject.ships.include? ship).to_be true
+    expect(subject.ships.include? ship).to eq true
     expect(subject.ships.count).to eq 1
   end
+
+  it "can receibe a hit on a ship" do
+  	ship = double :ship, :hit => nil, :position => [ 1, 1 ]
+  	subject.place ship
+  	expect( ship ).to receive( :hit )
+  	subject.hit 1, 1
+  end
+
 
 end
